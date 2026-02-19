0749abcd5a97ab840d75502e9a4caffc17636bac
Junseo Lee <junseo.lee@snu.ac.kr>
2026-02-19 10:22:45 +0900

    grep string

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,4 +4,7 @@ vim.keymap.set('n', '<leader>sg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>sG', builtin.git_files, {})
 vim.keymap.set('n', '<leader>sb', builtin.buffers, {})
 
+-- grep current word under cursor
+vim.keymap.set('n', '<leader>ss', builtin.grep_string, {})
+
 
